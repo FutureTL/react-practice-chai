91a7d431aa447003b0b9c244130ee4f98c04b4cd
FutureTL <futuretechnologistics21@gmail.com>
2025-06-19 13:20:38 +0530

    Hooks started and the working of useState hook explored

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+
+
+function App() {
+  
+  let [counter, setCounter]  = useState(15)// 15 represents the default value of the variable
+  //useState return an array where the 0th index place has the variable whose
+  //state is to be updated, 
+  //1th index denotes the function that is called to do the updation and required parameter is passed
+  
+  const addValue = ()=>{
+    if(counter <20){
+      counter = counter+1
+      setCounter(counter)
+      console.log(`${counter}, ${Math.random()}`)
+    }
+}
+
+const removeValue = ()=>{
+    if(counter>0){
+      counter = counter-1
+      setCounter(counter)
+      console.log(`${counter}, ${Math.random()}`)
+    }
+ 
+  }
+
+  return (
+    <>
+    <h1>Count: {counter}</h1>  
+    <button 
+      onClick = {addValue}
+    >Add value: {counter} </button>
+    <br/>
+    <button
+      onClick={removeValue}
+    >Remove value : {counter}</button>    
+    </>
+  )
+}
+
+export default App
