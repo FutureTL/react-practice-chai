8faca76a0bd822b77c46f7fb3cb30f128333cb70
FutureTL <futuretechnologistics21@gmail.com>
2025-07-04 16:02:46 +0530

    UserContext and UserContextProvide r created

---
+++ 08miniContext/src/Context/UserContext.js
@@ -0,0 +1,6 @@
+//this we have not kept as a .jsx file because pure javascript will be written in it.
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
